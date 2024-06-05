852b2b282f842d2592820c8808ab30d18a117551
ivalimak <ivalimak@student.hive.fi>
2024-06-05 14:09:42 +0300

    Added ex00

---
+++ ex00/inc/Fixed.hpp
@@ -0,0 +1,32 @@
+// ███████╗████████╗     ██████╗ ██╗   ██╗████████╗ ██████╗██╗  ██╗ █████╗ ██████╗
+// ██╔════╝╚══██╔══╝     ██╔══██╗██║   ██║╚══██╔══╝██╔════╝██║  ██║██╔══██╗██╔══██╗
+// █████╗     ██║        ██████╔╝██║   ██║   ██║   ██║     ███████║███████║██████╔╝
+// ██╔══╝     ██║        ██╔═══╝ ██║   ██║   ██║   ██║     ██╔══██║██╔══██║██╔══██╗
+// ██║        ██║███████╗██║     ╚██████╔╝   ██║   ╚██████╗██║  ██║██║  ██║██║  ██║
+// ╚═╝        ╚═╝╚══════╝╚═╝      ╚═════╝    ╚═╝    ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+//
+// <<Fixed.hpp>>
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class Fixed
+{
+	private:
+
+		static const int	_bits;
+		int					_value;
+
+	public:
+
+		Fixed(void);
+		Fixed(const Fixed &copy);
+		Fixed &operator=(const Fixed &copy);
+		~Fixed(void);
+
+		void	setRawBits(const int raw);
+
+		int		getRawBits(void) const;
+};
+
+#endif
---
+++ ex00/src/Fixed.cpp
@@ -0,0 +1,49 @@
+// ███████╗████████╗     ██████╗ ██╗   ██╗████████╗ ██████╗██╗  ██╗ █████╗ ██████╗
+// ██╔════╝╚══██╔══╝     ██╔══██╗██║   ██║╚══██╔══╝██╔════╝██║  ██║██╔══██╗██╔══██╗
+// █████╗     ██║        ██████╔╝██║   ██║   ██║   ██║     ███████║███████║██████╔╝
+// ██╔══╝     ██║        ██╔═══╝ ██║   ██║   ██║   ██║     ██╔══██║██╔══██║██╔══██╗
+// ██║        ██║███████╗██║     ╚██████╔╝   ██║   ╚██████╗██║  ██║██║  ██║██║  ██║
+// ╚═╝        ╚═╝╚══════╝╚═╝      ╚═════╝    ╚═╝    ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+//
+// <<Fixed.cpp>>
+
+#include <iostream>
+#include "Fixed.hpp"
+
+const int	Fixed::_bits = 8;
+
+Fixed::Fixed(void)
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->setRawBits(0);
+}
+
+Fixed::Fixed(const Fixed &copy)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Fixed &Fixed::operator=(const Fixed &copy)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->setRawBits(copy.getRawBits());
+	return *this;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+void	Fixed::setRawBits(const int raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_value = raw;
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->_value;
+}
